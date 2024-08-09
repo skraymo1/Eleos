f03abf6337c979fa7af428fd2f76b03e3caec607
skraymo1 <kyleraymond@microsoft.com>
2024-08-09 15:15:24 -0500

    adochanges

--- apim-chat-portal/src/components/chat/ChatBubble.tsx
+++
@@ -1,9 +0,0 @@
-const ChatBubble = ({ msg }) => {
-    return (
-        <div className={`bubble ${msg.participant === 'bot' ? 'bot' : 'user'}`}>
-            {msg.value}
-        </div>
-    );
-};
-
-export default ChatBubble;
